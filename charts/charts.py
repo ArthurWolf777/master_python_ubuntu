ba0d3e4c78d09264e3567ac3200fbec46c14a40d
Arthur <goldsmith.blacksmith@gmail.com>
2023-02-09 21:38:50 -0600

    pie generator

--- charts/charts.py
+++ charts/charts.py
@@ -1,3 +1,11 @@
-import matplotlib.pylot as pylot
+import matplotlib.pyplot as pyplot
 
-def
+def generate_pie_chart():
+    labels = ['A', 'B', 'C']
+    values = [200, 27, 90]
+
+
+    fig, ax = pyplot.subplots()
+    ax.pie(values, labels=labels)
+    pyplot.savefig('pie.png')
+    pyplot.close()
